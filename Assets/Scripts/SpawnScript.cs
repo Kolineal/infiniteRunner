c23657a19f4f9afd8b2675eed556fb286e77e10c
Kolineal <kolineal@gmail.com>
2014-03-04 19:16:32 +0200

    maze generator and gyro controls

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class SpawnScript : MonoBehaviour {
 
 	public GameObject[] obj;
+	public int spawnLimit;
+	public bool limited = false;
 	public float minTime = 1f;
 	public float maxTime = 2f;
 	// Use this for initialization
@@ -15,8 +17,16 @@ public class SpawnScript : MonoBehaviour {
 	
 	}
 	public void Spawn(){
-		Instantiate (obj[Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
-		Invoke ("Spawn", Random.Range (minTime, maxTime));
+		if (limited) {
+						if (spawnLimit > 0) {
+								Instantiate (obj [Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
+								Invoke ("Spawn", Random.Range (minTime, maxTime));
+								spawnLimit--;
+						}
+		} else {
+			Instantiate (obj [Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
+			Invoke ("Spawn", Random.Range (minTime, maxTime));
+		}
 	}
 
 }
